f6c90c9efcaa889ce46272e2577bfa0d7a092993
Ikezogwo Kosisochukwu <ikezogwokosiso16@gmail.com>
2024-12-29 22:35:20 +0100

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { Analytics } from "@vercel/analytics/react"
 import Index from "./pages/Index";
 import Events from "./pages/Events";
 import Directory from "./pages/Directory";
@@ -23,6 +24,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
+        <Analytics />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
